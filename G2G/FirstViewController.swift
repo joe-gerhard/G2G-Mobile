3b37b48147871a1e454e834cf0e72ae002da4de4
David Bee <baeddavid@gmail.com>
2019-09-21 11:36:00 -0500

    Initial Commit

---
+++ G2G/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  G2G
+//
+//  Created by David Bae on 9/21/19.
+//  Copyright © 2019 David Bae. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ G2G/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  G2G
+//
+//  Created by David Bae on 9/21/19.
+//  Copyright © 2019 David Bae. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
